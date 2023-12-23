b8689eea77dd47c9a74164b4a4acd728cc296ad7
rkkcss <galdani1997@gmail.com>
2023-12-23 19:01:47 +0100

    fix: user start/stop typing after refresh page

--- src/components/ChatMessageSection.jsx
+++ src/components/ChatMessageSection.jsx
@@ -35,12 +35,12 @@ export const ChatMessagesSection = () => {
     usePagination();
 
   useEffect(() => {
-    socket?.current.on("userStartTypingToClient", (res) => {
+    socket?.current?.on("userStartTypingToClient", (res) => {
       if (res.roomId == roomId && ourSelf.id != res.user.id) {
         setIsUserTyping(true);
       }
     });
-    socket?.current.on("userStopTypingToClient", () => {
+    socket?.current?.on("userStopTypingToClient", () => {
       setIsUserTyping(false);
     });
   }, [socket.current]);
@@ -76,7 +76,7 @@ export const ChatMessagesSection = () => {
   }, [roomId]);
 
   useEffect(() => {
-    socket?.current.emit("joinRoom", { roomId: roomId });
+    socket?.current?.emit("joinRoom", { roomId: roomId });
   }, [socket.current, roomId]);
 
   //save message to db and send to the group via socket
@@ -113,7 +113,7 @@ export const ChatMessagesSection = () => {
   const userTypeingHandler = (e) => {
     setMessageInput(e);
 
-    socket?.current.emit("userStartTypingToServer", {
+    socket?.current?.emit("userStartTypingToServer", {
       user: {
         id: ourSelf?.id,
       },
@@ -121,7 +121,7 @@ export const ChatMessagesSection = () => {
     });
 
     setTimeout(() => {
-      socket?.current.emit("userStopTypingToServer", {
+      socket?.current?.emit("userStopTypingToServer", {
         user: {
           id: ourSelf?.id,
         },
@@ -131,7 +131,7 @@ export const ChatMessagesSection = () => {
   };
 
   useEffect(() => {
-    socket?.current.on("groupMessageToClient", (data) => {
+    socket?.current?.on("groupMessageToClient", (data) => {
       setMessages(
         (prevMessages) => new Map([[data.id, data], ...prevMessages])
       );
@@ -146,76 +146,74 @@ export const ChatMessagesSection = () => {
   };
 
   return (
-    <>
-      <div className="h-full flex flex-col">
-        {/* Chat message section header component */}
-        <ChatMessageSectionHeader />
-        <div className="flex-grow p-6 overflow-y-auto relative flex flex-col-reverse gap-2">
-          {Array.from(messages.values()).map((message, i) =>
-            i + 1 === messages.size ? (
-              <Message
-                message={message}
-                key={message.id}
-                lastMessageRef={lastElementRef}
-              />
-            ) : (
-              <Message message={message} key={message.id} />
-            )
-          )}
-          <div className={`${isUserTyping ? "block" : "hidden"}`}>
-            <span className="flex items-center gap-2 left-3">
-              <img src={userImg} alt="" className="w-6 h-6 rounded-full" />
-              <TypeingDots />
-            </span>
-          </div>
+    <div className="h-full flex flex-col">
+      {/* Chat message section header component */}
+      <ChatMessageSectionHeader />
+      <div className="flex-grow p-6 overflow-y-auto relative flex flex-col-reverse gap-2">
+        {Array.from(messages.values()).map((message, i) =>
+          i + 1 === messages.size ? (
+            <Message
+              message={message}
+              key={message.id}
+              lastMessageRef={lastElementRef}
+            />
+          ) : (
+            <Message message={message} key={message.id} />
+          )
+        )}
+        <div className={`${isUserTyping ? "block" : "hidden"}`}>
+          <span className="flex items-center gap-2 left-3">
+            <img src={userImg} alt="" className="w-6 h-6 rounded-full" />
+            <TypeingDots />
+          </span>
         </div>
+      </div>
 
-        <div className="w-full block border-t px-6 pb-4 border-gray-300">
-          <div className="py-2 text-xl text-gray-400 gap-9 flex">
-            <div className="file-input-container flex items-center justify-center">
-              <label htmlFor="file-input" className="icon-container">
-                <FileImageOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
-              </label>
-              <input
-                type="file"
-                id="file-input"
-                className="hidden"
-                onChange={(event) =>
-                  handleImageMessage(event, event.target.files)
-                }
+      <div className="w-full block border-t px-6 pb-4 border-gray-300">
+        <div className="py-2 text-xl text-gray-400 gap-9 flex">
+          <div className="file-input-container flex items-center justify-center">
+            <label htmlFor="file-input" className="icon-container">
+              <FileImageOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
+            </label>
+            <input
+              type="file"
+              id="file-input"
+              className="hidden"
+              onChange={(event) =>
+                handleImageMessage(event, event.target.files)
+              }
+            />
+          </div>
+          <VideoCameraOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
+          <GifOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
+        </div>
+        <div className="flex flex-grow gap-5 justify-between items-center w-full">
+          {imageMessage ? (
+            <div className={`relative`}>
+              <CloseOutlined
+                className="hover:rounded-full absolute right-[-10px] top-[-10px] bg-gray-200 p-1 rounded-full hover:bg-gray-300 hover:cursor-pointer"
+                onClick={() => setImageMessage("")}
+              />
+              <img
+                src={URL.createObjectURL(imageMessage)}
+                alt="asd"
+                className="w-12 h-12 rounded-md"
               />
             </div>
-            <VideoCameraOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
-            <GifOutlined className="hover:text-emerald-400 hover:cursor-pointer py-2" />
-          </div>
-          <div className="flex flex-grow gap-5 justify-between items-center w-full">
-            {imageMessage ? (
-              <div className={`relative`}>
-                <CloseOutlined
-                  className="hover:rounded-full absolute right-[-10px] top-[-10px] bg-gray-200 p-1 rounded-full hover:bg-gray-300 hover:cursor-pointer"
-                  onClick={() => setImageMessage("")}
-                />
-                <img
-                  src={URL.createObjectURL(imageMessage)}
-                  alt="asd"
-                  className="w-12 h-12 rounded-md"
-                />
-              </div>
-            ) : (
-              <div className="w-full">
-                <InputEmoji
-                  onChange={userTypeingHandler}
-                  value={messageInput}
-                  placeholder={"Aa"}
-                />
-              </div>
-            )}
-            <div onClick={sendMessageAndSave}>
-              <Button type={"button"} variant={"primary"} text={"Küld"} />
+          ) : (
+            <div className="w-full">
+              <InputEmoji
+                onChange={userTypeingHandler}
+                value={messageInput}
+                placeholder={"Aa"}
+              />
             </div>
+          )}
+          <div onClick={sendMessageAndSave}>
+            <Button type={"button"} variant={"primary"} text={"Küld"} />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
